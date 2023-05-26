eb7c4fe41ea7981e8f8a5acad760e819a28057aa
minetake <minetake01.m@gmail.com>
2023-05-26 21:06:42 +0900

    mixin/MineAudioWorkstation.javaをmixin/Mixin.javaにファイル名変更。
    ファイル名を区別しやすくするため。

--- src/main/java/net/minetake/mineaudioworkstation/mixin/Mixin.java
+++ src/main/java/net/minetake/mineaudioworkstation/mixin/Mixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @org.spongepowered.asm.mixin.Mixin(TitleScreen.class)
-public class MineAudioWorkstation {
+public class Mixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
 		net.minetake.mineaudioworkstation.MineAudioWorkstation.LOGGER.info("This line is printed by an example mod mixin!");
