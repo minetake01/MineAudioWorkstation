99895d2b5c2f8bf8a258e2b24df3fa113db4667a
minetake <minetake01.m@gmail.com>
2023-06-02 16:02:03 +0900

    net.minecraft.state.property.PropertiesクラスのNOTE定数の最大値を24から48へ書き換え。
    BlockStateのnote値が最大48まで持てるように。

---
+++ src/main/java/net/minetake/mineaudioworkstation/mixin/NotePropertyMixin.java
@@ -0,0 +1,25 @@
+package net.minetake.mineaudioworkstation.mixin;
+
+import net.minecraft.state.property.Properties;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+@Mixin(Properties.class)
+public class NotePropertyMixin {
+    @ModifyConstant(
+        method = "<clinit>",
+        constant = @Constant(intValue = 24),
+        slice = @Slice(
+            from = @At(
+                value = "CONSTANT",
+                args = "stringValue=note"
+            )
+        )
+    )
+    private static int modifyNoteMaxValue(int original) {
+        return 48;
+    }
+}
